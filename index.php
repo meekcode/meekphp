7c62dc9c340817ebb00f8da367ceec72e297c9d0
Michael Edwards <meekcode.com@gmail.com>
2014-08-01 03:05:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
 
+/******************************************************************************/
+
+/** comment these out to disable errors **/
+
+ini_set('error_reporting', E_ALL);
+
+ini_set('display_errors', 'On'); 
+
+/** define file path on server for meekphp **/
+
+define("__PATH__", realpath(dirname(__FILE__)) . '/');
+
+/** run config file **/
+
+include(__PATH__ . 'config.php');
+
+/******************************************************************************/
+
